a4f637c4548934e1f60b957c887893186d374243
mars <imidcen@gmail.com>
2025-05-31 16:31:56 +0100

    lesson 45

--- frontEndDevelopmentLibraries/4-react/src/App.js
+++ frontEndDevelopmentLibraries/4-react/src/App.js
@@ -945,6 +945,29 @@ class MyToDoList extends React.Component {
 }
 
 
+// lesson 45
+const frontEndFrameworks = [
+  'React',
+  'Angular',
+  'Ember',
+  'Knockout',
+  'Backbone',
+  'Vue'
+];
+
+function Frameworks() {
+  const renderFrameworks = frontEndFrameworks.map(i => <li key={i}>{i}</li>);
+  return (
+    <div>
+      <h1>Popular Front End JavaScript Frameworks</h1>
+      <ul>
+        {renderFrameworks}
+      </ul>
+    </div>
+  );
+};
+
+
   return (
     <div>
       {JSX}
@@ -1007,6 +1030,8 @@ class MyToDoList extends React.Component {
       <GateKeeper />
 
       <MyToDoList />
+
+      <Frameworks />
     </div>
   );
 }
