bd9863035a4344da35b72aeaaf075298dd459952
mars <imidcen@gmail.com>
2025-05-31 16:09:05 +0100

    lesson 41

--- frontEndDevelopmentLibraries/4-react/src/App.js
+++ frontEndDevelopmentLibraries/4-react/src/App.js
@@ -774,6 +774,59 @@ class ConciseConditional extends React.Component {
 };
 
 
+// lesson 41
+const inputStyle2 = {
+  width: 235,
+  margin: 5
+};
+
+class CheckUserAge extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: '',
+      userAge: ''
+    }
+    this.submit = this.submit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(e) {
+    this.setState({
+      input: e.target.value,
+      userAge: ''
+    });
+  }
+  submit() {
+    this.setState(state => ({
+      userAge: state.input
+    }));
+  }
+  render() {
+    const buttonOne = <button onClick={this.submit}>Submit</button>;
+    const buttonTwo = <button>You May Enter</button>;
+    const buttonThree = <button>You Shall Not Pass</button>;
+    return (
+      <div>
+        <h3>Enter Your Age to Continue</h3>
+        <input
+          style={inputStyle2}
+          type="number"
+          value={this.state.input}
+          onChange={this.handleChange} />
+        <br />
+        {
+          this.state.userAge === ''
+            ? buttonOne
+            : this.state.userAge >= 18
+              ? buttonTwo
+              : buttonThree
+          }
+      </div>
+    );
+  }
+}
+
+
   return (
     <div>
       {JSX}
@@ -828,6 +881,8 @@ class ConciseConditional extends React.Component {
       <RenderIfElse />
 
       <ConciseConditional />
+
+      <CheckUserAge />
     </div>
   );
 }
