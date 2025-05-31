40471cf4b8d51979d48cad813660d14709c699fe
mars <imidcen@gmail.com>
2025-05-31 14:57:21 +0100

    lesson 29

--- frontEndDevelopmentLibraries/4-react/src/App.js
+++ frontEndDevelopmentLibraries/4-react/src/App.js
@@ -377,6 +377,42 @@ function App() {
   };
 
 
+  // lesson 29
+  class MyForm extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        input: '',
+        submit: ''
+      };
+      this.handleChange = this.handleChange.bind(this);
+      this.handleSubmit = this.handleSubmit.bind(this);
+    }
+    handleChange(event) {
+      this.setState({
+        input: event.target.value
+      });
+    }
+    handleSubmit(event) {
+      event.preventDefault()
+      this.setState({
+        submit: this.state.input
+      })
+    }
+    render() {
+      return (
+        <div>
+          <form onSubmit={this.handleSubmit}>
+            <input value={this.state.input} onChange={this.handleChange} />
+            <button type='submit'>Submit!</button>
+          </form>
+          <h1>{this.state.submit}</h1>
+        </div>
+      );
+    }
+  }
+
+
   return (
     <div>
       {JSX}
@@ -409,6 +445,8 @@ function App() {
       <Counter />
 
       <ControlledInput />
+
+      <MyForm />
     </div>
   );
 }
