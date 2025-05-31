0e8a8d8e89b55550382102d07b08b4b64d5dcc70
mars <imidcen@gmail.com>
2025-05-31 14:20:40 +0100

    lesson 24

--- frontEndDevelopmentLibraries/4-react/src/App.js
+++ frontEndDevelopmentLibraries/4-react/src/App.js
@@ -226,6 +226,32 @@ function App() {
   };
 
 
+  class SetState extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        name: 'Initial State'
+      };
+      this.handleClick = this.handleClick.bind(this);
+    }
+    handleClick() {
+      // Change code below this line
+      this.setState({
+        name: 'React Rocks!'
+      })
+      // Change code above this line
+    }
+    render() {
+      return (
+        <div>
+          <button onClick={this.handleClick}>Click Me</button>
+          <h1>{this.state.name}</h1>
+        </div>
+      );
+    }
+  };
+
+
   return (
     <div>
       {JSX}
@@ -248,6 +274,8 @@ function App() {
       <StatefulComponent />
 
       <RenderState />
+
+      <SetState />
     </div>
   );
 }
