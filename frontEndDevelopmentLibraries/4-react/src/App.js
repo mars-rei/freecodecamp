dd7a6b3b9d990fa175d299134dccd66d83399e33
mars <imidcen@gmail.com>
2025-05-31 15:30:36 +0100

    lesson 35

--- frontEndDevelopmentLibraries/4-react/src/App.js
+++ frontEndDevelopmentLibraries/4-react/src/App.js
@@ -525,6 +525,88 @@ function App() {
   }
 
 
+  // lesson 34
+
+  class EventListeners extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        message: ''
+      };
+      this.handleEnter = this.handleEnter.bind(this);
+      this.handleKeyPress = this.handleKeyPress.bind(this);
+    }
+    // Change code below this line
+    componentDidMount() {
+      document.addEventListener('keydown', this.handleKeyPress)
+    }
+    componentWillUnmount() {
+      document.removeEventListener('keydown', this.handleKeyPress)
+    }
+    // Change code above this line
+    handleEnter() {
+      this.setState((state) => ({
+        message: state.message + 'You pressed the enter key! '
+      }));
+    }
+    handleKeyPress(event) {
+      if (event.keyCode === 13) {
+        this.handleEnter();
+      }
+    }
+    render() {
+      return (
+        <div>
+          <h1>{this.state.message}</h1>
+        </div>
+      );
+    }
+  };
+
+
+  // lesson 35 
+  class OnlyEvens extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log('Should I update?');
+    if (nextProps.value % 2 == 0 ) {
+      return true;
+    }
+    // Change code above this line
+  }
+  componentDidUpdate() {
+    console.log('Component re-rendered.');
+  }
+  render() {
+    return <h1>{this.props.value}</h1>;
+  }
+}
+
+class Controller extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: 0
+    };
+    this.addValue = this.addValue.bind(this);
+  }
+  addValue() {
+    this.setState(state => ({
+      value: state.value + 1
+    }));
+  }
+  render() {
+    return (
+      <div>
+        <button onClick={this.addValue}>Add</button>
+        <OnlyEvens value={this.state.value} />
+      </div>
+    );
+  }
+}
+
   return (
     <div>
       {JSX}
@@ -565,6 +647,10 @@ function App() {
       <PassCallbackProps />
 
       <ComponentDidMount />
+
+      <EventListeners />
+
+      <Controller />
     </div>
   );
 }
